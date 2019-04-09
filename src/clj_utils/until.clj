ce3d87d5ec9ad8b399a07d0000a11a099b6d4ed2
Eugene Tyurin <eugene@tyurin.com>
2019-04-08 23:02:13 -0500

    clj-utils.until

---
+++ src/clj_utils/until.clj
@@ -0,0 +1,63 @@
+(ns clj-utils.until)
+
+;; The following code is taken from Clojure submissions, it was never
+;; adopted into clojure.core:
+;; https://dev.clojure.org/jira/browse/CLJ-1451
+
+(defn take-until
+  "Returns a lazy sequence of successive items from coll until
+  (pred item) returns true, including that item. pred must be
+  free of side-effects. Returns a transducer when no collection
+  is provided."
+  {;:added "1.7"
+   :static true}
+  ([pred]
+   (fn [rf]
+     (fn
+       ([] (rf))
+       ([result] (rf result))
+       ([result input]
+        (if (pred input)
+          (ensure-reduced (rf result input))
+          (rf result input))))))
+  ([pred coll]
+   (lazy-seq
+    (when-let [s (seq coll)]
+      (if (pred (first s))
+        (cons (first s) nil)
+        (cons (first s) (take-until pred (rest s))))))))
+
+
+;; The following code is adopted from clojure.core/partiion-by and partition-all
+
+(defn partition-until
+  "Applies pred to each value in coll, splitting it each time pred returns true.
+  Returns a lazy seq of partitions.  Returns a stateful transducer when
+  no collection is provided."
+  {;:added "1.2"
+   :static true}
+  ([pred]
+   (fn [rf]
+     (let [a (java.util.ArrayList.)]
+       (fn
+         ([] (rf))
+         ([result]
+          (let [result (if (.isEmpty a)
+                         result
+                         (let [v (vec (.toArray a))]
+                           ;;clear first!
+                           (.clear a)
+                           (unreduced (rf result v))))]
+            (rf result)))
+         ([result input]
+          (.add a input)
+          (if (pred input)
+            (let [v (vec (.toArray a))]
+              (.clear a)
+              (rf result v))
+            result))))))
+  ([pred coll]
+   (lazy-seq
+    (when-let [s (seq coll)]
+      (let [run (take-until pred s)]
+        (cons run (partition-until pred (seq (drop (count run) s)))))))))
---
+++ test/clj_utils/until_test.clj
@@ -0,0 +1,24 @@
+(ns clj-utils.until-test
+  (:use [clojure.test])
+  (:require [clj-utils.until :as u :refer :all]))
+
+
+(deftest take-until-test
+  (testing "take-until"
+    (is (true? (= [:a] (u/take-until #{:a} [:a :b :c]))))
+    (is (true? (= [:a :b] (u/take-until #{:b} [:a :b :c]))))
+    (is (true? (= '(1 2 -1) (u/take-until neg? '(1 2 -1)))))
+    (is (true? (= '() (u/take-until pos? '()))))))
+
+
+(deftest partition-until-test
+  (testing "partition-until in a functional form"
+    (let [s1 [:a :b :c :a :b :d :d :e]]
+      (is (true? (= '((:a :b :c) (:a :b :d) (:d) (:e))
+                    (u/partition-until #{:c :d} s1)))))))
+
+(deftest partition-until-xf-test
+  (testing "partition-until in a transducer form"
+    (let [xfp (u/partition-until pos?)]
+      (is (true? (= '([1] [-1 -2 1] [1] :a)
+                      (transduce xfp conj '(:a) [1 -1 -2 1 1])))))))
